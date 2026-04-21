5a2144d126ad04644702ba435a242fa2d1b3ad1a
gangyao93 <yaoisyao@163.com>
2026-04-21 15:25:18 -0300

    ADD: ansible dynamic test function

--- service/device_service.py
+++ service/device_service.py
@@ -1,12 +1,81 @@
+from pathlib import Path
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 from schemas.response import Response
 from VO.DeviceVO import DeviceRegister
 from entity.device_info import DeviceInfo
 import logging
-
+import ansible_runner
 
 log = logging.getLogger("device_service")
+
+data = {
+    "0c:67:0d:13:00:00": {
+        "interfaces": [
+            {
+                "name": "eth1",
+                "address": "192.168.12.1/24"
+            },
+            {
+                "name": "eth2",
+                "address": "192.168.13.1/24"
+            }
+        ],
+        "ospf_areas": [
+            {
+                "area": "0",
+                "networks": [
+                    "192.168.12.0/24",
+                    "192.168.13.0/24"
+                ]
+            }
+        ]
+    },
+    "0c:e7:2f:0d:00:00": {
+        "interfaces": [
+            {
+                "name": "eth1",
+                "address": "192.168.12.1/24"
+            },
+            {
+                "name": "eth2",
+                "address": "192.168.23.1/24"
+            }
+        ],
+        "ospf_areas": [
+            {
+                "area": "0",
+                "networks": [
+                    "192.168.12.0/24"
+                    "192.168.23.0/24"
+                ]
+            }
+        ]
+    },
+    "0c:21:dc:c6:00:00": {
+        "interfaces": [
+            {
+                "name": "eth1",
+                "address": "192.168.13.1/24"
+            },
+            {
+                "name": "eth4",
+                "address": "192.168.23.1/24"
+            }
+        ],
+        "ospf_areas": [
+            {
+                "area": "0",
+                "networks": [
+                    "192.168.23.0/24"
+                    "192.168.13.0/24"
+                ]
+            }
+        ]
+    }
+}
+
 
 async def register_device(device: DeviceRegister, db: AsyncSession):
     device_info = DeviceInfo(**device.model_dump())
@@ -27,3 +96,38 @@ async def register_device(device: DeviceRegister, db: AsyncSession):
         return Response.success({"id": device_info.id})
     except Exception as e:
         return Response.fail({"error": str(e)})
+
+
+async def ansible_test(mac: str, ip_address: str):
+
+    conf = data[mac]
+    if not conf:
+        return Response.fail(f"{mac} not registered")
+    base_dir = Path(__file__).resolve().parent.parent
+    playbook_name = "test_playbook.yml"
+    playbook_path = base_dir / "playbook" / playbook_name
+    print(playbook_path)
+    r = ansible_runner.run(
+        private_data_dir=str(base_dir),
+        playbook=str(playbook_path),
+        inventory={
+            "all": {
+                "hosts": {
+                    "vyos1": {
+                        "ansible_host": ip_address
+                    }
+                }
+            }
+        },
+        extravars={
+            "ansible_user": "admin",
+            "ansible_ssh_pass": "password",
+            "ansible_connection": "network_cli",
+            "ansible_network_os": "vyos.vyos.vyos",
+            "vlan_desc": "USER_VLAN",
+            **conf
+        }
+    )
+    if r.rc != 0:
+        return Response.fail(r.stderr)
+    return Response.success(r.stdout)
